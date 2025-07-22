c3126fba22f5ce7de4288d23c048b05cf586234e
Anh Pham <anhsmacbook@Anhs-MacBook-Pro.local>
2025-07-21 23:34:41 -0700

    Set up Firebase for realtime DB, loaded menu/about info into Pinecone, initialized vector search with cosine similarity, started system design

---
+++ python_code/api/agent/utils.py
@@ -0,0 +1,26 @@
+def get_chatbot_response(client, model_name, messages, temperature=0.0):
+    input_messages = []
+
+    for message in messages:
+        input_messages.append({
+            "role": message["role"],
+            "content": message["content"]
+        })
+
+    response = client.chat.completions.create(
+        model=model_name,
+        messages=input_messages,
+        temperature=0.0,
+        top_p=0.8,
+        max_tokens=2000
+    ).choices[0].message.content
+    return response
+
+def get_embedding(embedding_client, model_name, text_input):
+    output = embedding_client.embeddings.create(input=text_input, model=model_name)
+
+    embeddings = []
+    for embedding_ocject in output.data:
+      embeddings.append(embedding_ocject.embedding)
+
+    return embeddings
